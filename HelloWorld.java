a9d5e3312ae0f4bf7dc88d2adcbae61097c71eb6
Anantaraj Sajjan <anantaraj.sajjan@tavant.com>
2017-07-19 15:42:45 +0530

    Commiting HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("***********************************************"+new Date());
+		System.out.println("Hello world code");
+		System.out.println("Welcome to Jenkins sample code");
+		System.out.println("************************************************");
+
+	}
+
+}
